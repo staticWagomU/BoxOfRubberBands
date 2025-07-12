4e901227ddbed9665b06da7b083e748e5dacac0f
staticWagomU <to6wa17@gmail.com>
2025-07-12 15:29:39 +0900

    chore(daily): wai-ariaを追加

--- src/pages/daily/[...path].ts
+++ src/pages/daily/[...path].ts
@@ -23,26 +23,35 @@ export const { getStaticPaths, GET } = OGImageRoute({
 				imagePath = `${year}/${month}/${filename}`;
 			}
 		}
-		
+
 		// Use the updated image path for OG image generation
 		// The file will be saved at /daily/yyyy/mm/yyyy-mm-dd.png
 		// Update path param to use the new format
 		path = imagePath;
 		return {
 			title: ` ${Number(page.pubDate.getMonth() + 1)}/${page.pubDate.getDate()}のにっき`,
+			description: "                                 輪ごむの空き箱",
 			bgImage: {
-				path: "./public/OGP/daily.png",
+				path: "./public/OGP/daily2.JPG",
 				fit: "cover",
 			},
 			font: {
 				title: {
 					size: 160,
 					color: [51, 51, 51],
-					families: ["Cherry Bomb One"],
+					// families: ["Cherry Bomb One"],
+					families: ["wagomufont"],
 					lineHeight: 2.3,
 				},
+				description: {
+					size: 80,
+					color: [51, 51, 51],
+					families: ["wagomufont"],
+					lineHeight: 1.5,
+				},
 			},
-			fonts: ["./src/pages/daily/_fonts/Cherry_Bomb_One/CherryBombOne-Regular.ttf"],
+			// fonts: ["./src/pages/daily/_fonts/Cherry_Bomb_One/CherryBombOne-Regular.ttf"],
+			fonts: ["./src/pages/daily/_fonts/wagomu-font/wagomufont.woff2"],
 		};
 	},
 });
